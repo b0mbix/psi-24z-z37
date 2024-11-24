8e8ef1602343e88d8837e60c510c82894df1cfa0
Your Name <asz-02@wp.pl>
2024-11-24 08:07:23 +0100

    added basic docker-compose.yaml to run our containers, didn't modify network parameters

---
+++ zadanie1/zadanie_1_2/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker rm -f z37_zadanie1_2_python_client
+docker rmi -f z37_zadanie1_2_python_client
+docker rm -f z37_zadanie1_2_python_server
+docker rmi -f z37_zadanie1_2_python_server
