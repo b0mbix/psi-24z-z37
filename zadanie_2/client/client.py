43b4aab3a3af8c27073786f06705032b732bb9ba
Angelika Ostrowska <01178572@pw.edu.pl>
2024-11-30 15:49:09 +0100

    Modified client code to use TCP

--- zadanie_2/client/client.py
+++ zadanie_2/client/client.py
@@ -1,10 +1,8 @@
+
 import socket
 import sys
 
-HOST = 'z37_zadanie1_1_python_server'
-# HOST = 'z37_zadanie1_1_c_server'
-sizes = [2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,40000,48000,56000,60000,64000,65000,65200,65400,65500,65502,65504,65505,65506,65507,65508]
-BUFSIZE = 1024
+HOST = 'z37_zadanie2_python_server'
 port = 8000
 
 if len(sys.argv) >= 2:
@@ -12,14 +10,17 @@ if len(sys.argv) >= 2:
 if len(sys.argv) >= 3:
 	port = int(sys.argv[2])
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Starting client job...")
-    for size in sizes:
-        msg_length = size - 2
-        msg = ''.join([chr(65 + i % 26) for i in range(msg_length)])
-        datagram = size.to_bytes(2, byteorder = 'big') + msg.encode('ascii')
 
-        s.sendto(datagram, (HOST, port))
+    s.connect((HOST, port))
 
-        data, _ = s.recvfrom(size)
-        print(f"Successfully sent datagram of size {size}")
+    msg_length = 100000
+    msg = ''.join([chr(65 + i % 26) for i in range(msg_length)])
+    stream = msg.encode('ascii')
+
+    s.sendall(stream)
+
+    data = s.recv(1024)
+    print('Received', repr(data))
+    print('Client finished.')
