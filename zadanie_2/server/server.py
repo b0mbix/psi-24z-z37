9fb9f45e9fa8ea2b15e4495a85ce7883c9b12999
Angelika Ostrowska <01178572@pw.edu.pl>
2024-11-30 16:10:30 +0100

    Modified server code to use TCP

--- zadanie_2/server/server.py
+++ zadanie_2/server/server.py
@@ -1,8 +1,9 @@
+
 import socket
 import sys
 
-HOST = '0.0.0.0' #'z37_zadanie1_1_python_server'
-BUFSIZE = 1024
+HOST = '0.0.0.0' #'z37_zadanie2_python_server'
+BUFSIZE = 100000
 port = 8000
 
 if len(sys.argv) >= 2:
@@ -10,22 +11,28 @@ if len(sys.argv) >= 2:
 
 print(f"Listening on {HOST}:{port}")
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind((HOST, port))
-	i = 1
+	# the number of unaccepted connections that the system will allow before refusing new connections
+	conn_nr = 5
+	s.listen(conn_nr)
+
 	while True:
-		data, address = s.recvfrom(BUFSIZE)
-		#print(data)
-		#print(address)
+		conn, addr = s.accept()
+		with conn:
+			print('Connect from: ', addr)
+			i=1
+			while True:
+				data = conn.recv( BUFSIZE )
 
-		if not data:
-			print("Error in datagram?")
-			s.sendto("Error in datagram?".encode('ascii'), address)
-			break
+				if not data:
+					print("Error in stream?")
+					conn.sendall("Error in datagram?".encode('ascii'))
+					break
 
-		length = int.from_bytes(data[:2], byteorder = 'big')
+				print(f"Received message no {i}")
+				conn.sendall(f"Received message no {i}".encode('ascii'))
+				i+=1
+		conn.close()
 
-		print(f"Received message no {i} of size {length}")
-		s.sendto(f"Received message no {i} of size {length}".encode('ascii'), address)
-
-		i += 1
+print("Connection closed by client" )
